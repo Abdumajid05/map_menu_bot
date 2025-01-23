daf664b5a7c5662b11bc084d2a75ad00ca03565b
Abdumajid05 <abdumajidabdulazizov513@gmail.com>
2025-01-23 16:12:45 +0500

    internal keyboards

--- main.py
+++ main.py
@@ -14,9 +14,77 @@ def start(update, context):
     ]
     reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=False)
     update.message.reply_text('Xush kelibsiz!', reply_markup=reply_markup)
-updater=Updater(TOKEN, use_context=True)
-dispatcher=updater.dispatcher
-start_handler=CommandHandler('start', start)
+def tarix(update, context):
+    keyboard = [
+        ['Abulfayzxon 📖'],
+        ['Akbarnoma 📚'],
+        ['Qobusnoma 📚'],
+        ['Ortga 🔙']
+    ]
+    reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=False)
+    update.message.reply_text('Tarixni tanlang:', reply_markup=reply_markup)
+
+def matematika(update, context):
+    keyboard = [
+        ['Axborotnoma 📘'],
+        ['Oliy matematika asoslari 📙'],
+        ['Differensial tenglamalar 📒'],
+        ['Ortga 🔙']
+    ]
+    reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=False)
+    update.message.reply_text('Matematika kitoblaridan tanlang:', reply_markup=reply_markup)
+
+def it(update, context):
+    keyboard = [
+        ['Python 🐍'],
+        ['JavaScript asoslari 🌐'],
+        ['HTML-CSS 🌍'],
+        ['Ortga 🔙']
+    ]
+    reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=False)
+    update.message.reply_text('IT kitoblaridan birini tanlang:', reply_markup=reply_markup)
+
+def english(update, context):
+    keyboard = [
+        ['The Great Gatsby 📖'],
+        ['Pride and Prejudice 📚'],
+        ['To Kill a Mockingbird 📚'],
+        ['Ortga 🔙']
+    ]
+    reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=False)
+    update.message.reply_text('English kitoblaridan birini tanlang:', reply_markup=reply_markup)
+
+def russian(update, context):
+    keyboard = [
+        ['Зеленая миля 📖'],
+        ['Книжный вор 📚'],
+        ['Гарри Поттер и Кубок огня 📘'],
+        ['Ortga 🔙']
+    ]
+    reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=False)
+    update.message.reply_text('Русский kitoblaridan birini tanlang:', reply_markup=reply_markup)
+
+def back_(update, context):
+    start(update, context)  
+
+updater = Updater(TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+start_handler = CommandHandler('start', start)
+tarix_handler = MessageHandler(Filters.regex('Tarix 📜'), tarix)
+matematika_handler = MessageHandler(Filters.regex('Matematika 📐'), matematika)
+it_handler = MessageHandler(Filters.regex('IT 💻'), it)
+english_handler = MessageHandler(Filters.regex('English 🇬🇧'), english)
+russian_handler = MessageHandler(Filters.regex('Русский 🇷🇺'), russian)
+back_handler = MessageHandler(Filters.regex('Ortga 🔙'), back_)
+
 dispatcher.add_handler(start_handler)
-updater.start_polling()
+dispatcher.add_handler(tarix_handler)
+dispatcher.add_handler(matematika_handler)
+dispatcher.add_handler(it_handler)
+dispatcher.add_handler(english_handler)
+dispatcher.add_handler(russian_handler)
+dispatcher.add_handler(back_handler)
+
+updater.start_polling() 
 updater.idle()
